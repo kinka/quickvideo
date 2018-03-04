fca340c6d6a7493f2582261654ebf0135c431d49
Kinka Huang <kinkabrain@gmail.com>
2018-03-04 10:16:20 +0800

    一键拉起微信并自动发起视频

---
+++ app/src/main/java/im/logger/quickvideo/AccessibilityService.java
@@ -0,0 +1,197 @@
+package im.logger.quickvideo;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static im.logger.quickvideo.Home.PREFS_NAME;
+
+public class AccessibilityService extends BaseAccessibilityService {
+
+    public final static String MM_PNAME = "com.tencent.mm";
+    private int hasVideo = 0;
+    private boolean onekey = false;
+    private boolean searching = false;
+    private String nickname = "Kinka";
+
+    private final static String Tag = "kk";
+    @Override
+    public void onAccessibilityEvent(final AccessibilityEvent event) {
+        int eventType = event.getEventType();
+
+        AccessibilityNodeInfo rootNode = getRootInActiveWindow();
+
+        switch (eventType) {
+            case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED:
+                if (!onekey) break;
+
+                if (rootNode == null) break;
+                if (!rootNode.getPackageName().toString().equals(MM_PNAME)) break;
+
+                if (searching) {
+                    List<AccessibilityNodeInfo> nodeList = rootNode.findAccessibilityNodeInfosByText(nickname);
+                    if (nodeList.size() > 1) { // 出现了搜索结果
+//                        Toast.makeText(getApplicationContext(), nodeList.size() + " size", Toast.LENGTH_SHORT).show();
+                        AccessibilityNodeInfo target = nodeList.get(0);
+                        target.getParent().performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                        searching = false;
+                    }
+                }
+
+                if (hasVideo > 1) {
+                    int count = 0;
+                    List<AccessibilityNodeInfo> nodeList = rootNode.findAccessibilityNodeInfosByText("聊天结束");
+                    count += nodeList.size();
+                    nodeList = rootNode.findAccessibilityNodeInfosByText("聊天已取消");
+                    count += nodeList.size();
+                    nodeList = rootNode.findAccessibilityNodeInfosByText("对方拒绝了你");
+                    count += nodeList.size();
+                    if (count > 0) {
+                        Toast.makeText(getApplicationContext(), "视频结束", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    if (hasVideo == 0) findButton(rootNode, "更多功能按钮，已折叠", true);
+                    findMoreButton(rootNode);
+                }
+                break;
+
+            case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
+                String pkgName = event.getPackageName().toString();
+                String clsName = event.getClassName().toString();
+//                Toast.makeText(getApplicationContext(), clsName + "_" + pkgName, Toast.LENGTH_SHORT).show();
+
+                if (pkgName.startsWith(MM_PNAME)) { // 微信的界面
+
+                    Object[] vs = getValue();
+                    nickname = (String) vs[1];
+
+                    if (!onekey && (Boolean) vs[0]) {
+                        onekey = true;
+                        setValue(false);
+
+                        findButton(rootNode, "搜索", true);
+                    }
+
+                    if (clsName.contains("com.tencent.mm.plugin.search.ui.FTSMainUI")) { // 搜索界面
+                        findEditText(rootNode, nickname);
+                        searching = true;
+                    }
+                } else {
+                    hasVideo = 0;
+                    onekey = false;
+                }
+                break;
+        }
+    }
+
+    private void setValue(boolean v) {
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putBoolean("onekey", v);
+
+        editor.commit();
+    }
+
+    private Object[] getValue() {
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        Object[] res = new Object[]{
+                settings.getBoolean("onekey", false),
+                settings.getString("nickname", "")
+        };
+
+        return res;
+    }
+
+    private boolean findEditText(AccessibilityNodeInfo rootNode, String content) {
+        int count = rootNode.getChildCount();
+        for (int i = 0; i < count; i++) {
+            AccessibilityNodeInfo nodeInfo = rootNode.getChild(i);
+            if (nodeInfo == null) {
+                continue;
+            }
+            CharSequence desc = nodeInfo.getContentDescription();
+            String strDesc = null;
+            if (desc != null) {
+                strDesc = desc.toString();
+                Log.i(Tag, strDesc);
+            }
+
+
+            if ("android.widget.EditText".equals(nodeInfo.getClassName())) {
+                Bundle arguments = new Bundle();
+                arguments.putInt(AccessibilityNodeInfo.ACTION_ARGUMENT_MOVEMENT_GRANULARITY_INT,
+                        AccessibilityNodeInfo.MOVEMENT_GRANULARITY_WORD);
+                arguments.putBoolean(AccessibilityNodeInfo.ACTION_ARGUMENT_EXTEND_SELECTION_BOOLEAN, true);
+                nodeInfo.performAction(AccessibilityNodeInfo.ACTION_PREVIOUS_AT_MOVEMENT_GRANULARITY, arguments);
+                nodeInfo.performAction(AccessibilityNodeInfo.ACTION_FOCUS);
+                ClipData clip = ClipData.newPlainText("label", content);
+                ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                clipboardManager.setPrimaryClip(clip);
+                nodeInfo.performAction(AccessibilityNodeInfo.ACTION_PASTE);
+                return true;
+            }
+            if (findEditText(nodeInfo, content)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    ArrayList clsList = new ArrayList();
+    private AccessibilityNodeInfo findButton(AccessibilityNodeInfo rootNode, String which, boolean shouldClick) {
+        if (clsList.size() == 0) {
+            clsList.add("android.widget.ImageButton");
+            clsList.add("android.widget.RelativeLayout");
+            clsList.add("android.widget.TextView");
+            clsList.add("android.widget.EditText");
+        }
+
+        int count = rootNode.getChildCount();
+        for (int i=0; i<count; i++) {
+            AccessibilityNodeInfo nodeInfo = rootNode.getChild(i);
+            if (nodeInfo == null) continue;
+
+            CharSequence rawDesc = nodeInfo.getContentDescription();
+            String desc = null;
+            if (rawDesc != null) {
+                desc = rawDesc.toString();
+                Log.i(Tag, String.format("desc: %s=>%s", desc, nodeInfo.getClassName()));
+
+                String clsName = nodeInfo.getClassName().toString();
+
+                boolean shouldCheck = clsList.indexOf(clsName) > -1;
+                if (shouldCheck && desc.contains(which)) {
+//                    Toast.makeText(getApplicationContext(), desc, Toast.LENGTH_SHORT).show();
+                    if (shouldClick) nodeInfo.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                    return nodeInfo;
+                }
+            }
+
+            if (findButton(nodeInfo, which, shouldClick) != null) return nodeInfo;
+        }
+
+        return null;
+    }
+
+    private boolean findMoreButton(AccessibilityNodeInfo rootNode) {
+        List<AccessibilityNodeInfo> nodeList = rootNode.findAccessibilityNodeInfosByText("视频聊天");
+        if (nodeList.size() > 0) {
+            AccessibilityNodeInfo videoChat = nodeList.get(0);
+            AccessibilityNodeInfo parent = videoChat.getParent();
+            parent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            hasVideo += 1;
+            return true;
+        }
+
+        return false;
+    }
+}
--- app/src/main/java/im/logger/quickvideo/Home.java
+++ app/src/main/java/im/logger/quickvideo/Home.java
@@ -1,5 +1,8 @@
 package im.logger.quickvideo;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +11,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class Home extends AppCompatActivity {
+
+    public static final String PREFS_NAME = "wechat_video";
+
+    private BaseAccessibilityService as = null;
+    private EditText nickName = null;
+    private Button btnOpenApp = null;
+    private PackageManager mPackageManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +37,47 @@ public class Home extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                as.goAccess();
             }
         });
+
+        mPackageManager = getPackageManager();
+
+        as = AccessibilityService.getInstance();
+        as.init(this);
+
+        nickName = (EditText)findViewById(R.id.nick_name);
+        btnOpenApp = (Button)findViewById(R.id.open_app);
+
+        btnOpenApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openApp();
+            }
+        });
+    }
+
+    private void openApp() {
+        String strNickName = nickName.getText().toString();
+        Toast.makeText(getApplicationContext(), strNickName, Toast.LENGTH_SHORT).show();
+
+        Intent intent = mPackageManager.getLaunchIntentForPackage(AccessibilityService.MM_PNAME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        setValue(true, strNickName);
+
+        startActivity(intent);
+    }
+
+    private void setValue(boolean v, String nickname) {
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putBoolean("onekey", v);
+        editor.putString("nickname", nickname);
+
+        editor.commit();
     }
 
     @Override
